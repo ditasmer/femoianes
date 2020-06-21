ea68ff703444c859e279b41cc4ced8a910aa8ef5
ditasmer <ditasmer@gmail.com>
2020-06-21 04:06:06 +0200

    refactor play video funtion

--- js/scripts.js
+++ js/scripts.js
@@ -4,34 +4,10 @@ pos.addEventListener("mousemove", (e) => {
   pos.style.setProperty("--y", e.clientY + "px");
 });
 
-function myFunction1() {
-  console.log(myFunction1);
-  var x = document.getElementById("myDIV1");
-  var v = document.getElementById("video1");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-    v.autoplay = true;
-    v.load();
-  } else {
-    x.style.display = "none";
-    v.pause();
-  }
-}
-function myFunction2() {
-  var x = document.getElementById("myDIV2");
-  var v = document.getElementById("video2");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-    v.autoplay = true;
-    v.load();
-  } else {
-    x.style.display = "none";
-    v.pause();
-  }
-}
-function myFunction3() {
-  var x = document.getElementById("myDIV3");
-  var v = document.getElementById("video3");
+function playVideo(nom_video, nom_div) {
+  console.log(playVideo);
+  var x = document.getElementById(nom_div);
+  var v = document.getElementById(nom_video);
   if (x.style.display === "none") {
     x.style.display = "block";
     v.autoplay = true;
