557ca2d6c693ce89d5c1ac26d64047d2427f7e40
ditasmer <ditasmer@gmail.com>
2020-07-03 18:35:35 +0200

    json data refresh

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
+/**cursor circle performance**/
 var pos = document.documentElement;
 pos.addEventListener("mousemove", (e) => {
   pos.style.setProperty("--x", e.clientX + "px");
   pos.style.setProperty("--y", e.clientY + "px");
 });
 
+/**play video and player window open/close**/
 function playVideo(nom_video, nom_div) {
-  console.log(playVideo);
+  console.log("playVideo");
   var x = document.getElementById(nom_div);
   var v = document.getElementById(nom_video);
   if (x.style.display === "none") {
@@ -18,8 +20,7 @@ function playVideo(nom_video, nom_div) {
   }
 }
 
-
-
+/**fetch list from json file**/
 function getMediaList() {
   // fetch("http://mernatus.com/shared/media-list-moianes.json", {
   //   mode: "no-cors",
@@ -31,35 +32,49 @@ function getMediaList() {
     fetch("media-list-moianes.json")
     .then((response) => response.json())
     .then((data) => {
+      console.log("getMediaList");
       tableMedias(data);
     })
     .catch((error) => console.error(error));
 }
 
-function tableMedias(data) {
-  var contenido = document.querySelector('#content-media');
-  console.log(data);
-  console.log('hola');
-  contenido.innerHTML = '';
-  for(let val of data){
-    contenido.innerHTML += `
-    <tr>
-      <th scope="row">${ val.id }</th>
-      <td>${ val.title }</td>
-      <td>${ val.autor }</td>
-    </tr>
+/**init table with empty data**/
+function tableMedias(someData) {
+  console.log("tableMedias");
+  //var contenido = document.querySelector('#content-media');
+  console.log(someData);
+  // contenido.innerHTML = '';
+  // for(let val of someData){
+  //   contenido.innerHTML += `
+  //   <tr>
+  //     <th scope="row">${ val.id }</th>
+  //     <td>${ val.title }</td>
+  //     <td>${ val.autor }</td>
+  //   </tr>
     
-    `;
+  //   `;
+  // }
+  let body_list = document.getElementById("body_list");
+  while (body_list.rows.length > 0) {
+    body_list.deleteRow(0);
   }
+  this.medias.forEach((media) => {
+    let fila = body_list.insertRow(body_list.rows.length);
+    fila.insertCell(0).innerHTML = media.nombre;
+    fila.insertCell(1).innerHTML = media.edad;
+  });
 }
 
+/**class filter searcher**/
 class SearchMedias {
   constructor() {
     this.medias = [
-      { nombre: "fr", edad: 23 },
-      { nombre: "frasdf", edad: 22 },
-      { nombre: "frou", edad: 2 },
+      { id: 1, title: "...", filename: "---", autor: "+++" },
+      { id: 2, title: "...", filename: "---", autor: "+++" },
+      { id: 3, title: "...", filename: "---", autor: "+++" },
+
     ];
+    console.log("SearchMedias constructor");
     console.log(this.medias);
     this.mediasBK = this.medias;
     this.onInit();
@@ -67,23 +82,34 @@ class SearchMedias {
   }
 
   onInit() {
+    console.log("onInit");
     let body_list = document.getElementById("body_list");
     while (body_list.rows.length > 0) {
       body_list.deleteRow(0);
     }
     this.medias.forEach((media) => {
       let fila = body_list.insertRow(body_list.rows.length);
-      fila.insertCell(0).innerHTML = media.nombre;
-      fila.insertCell(1).innerHTML = media.edad;
+      fila.insertCell(0).innerHTML = media.id;
+      fila.insertCell(1).innerHTML = media.title;
     });
   }
   search(id) {
+    console.log("search");
     let key_word = document.getElementById(id).value;
     this.medias = this.mediasBK;
+    let res = false;
     this.medias = this.medias.filter((media) => {
       //return -1 when match
-      return media.nombre.toLowerCase().indexOf(key_word) > -1;
+      return media.title.toLowerCase().indexOf(key_word.toLowerCase()) > -1;
     });
+    
+
+    this.onInit();
+  }
+ 
+  refresh(dataJson){
+    this.medias = dataJson;
+    this.mediasBK = this.medias;
     this.onInit();
   }
 }
@@ -92,3 +118,22 @@ let form = document.getElementById("searchForm");
 form.addEventListener("submit", () => {
   searcher.search("new_word");
 });
+
+/**init local json data onload**/
+const promiseOfSomeJsonData = fetch("media-list-moianes.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("in async");
+    console.log(data);
+    return data;
+  })
+  .catch((error) => console.error(error));
+  
+window.onload = async () => {
+  let dataJson = await promiseOfSomeJsonData;
+  console.log("onload");
+  console.log(dataJson);
+
+  //refresh searcher medias and mediasBK
+  searcher.refresh(dataJson);
+};
